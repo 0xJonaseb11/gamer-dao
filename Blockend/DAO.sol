fb97dbcd260ba15620fa62da6e5d33090d88683f
jonas-sebera <sebejaz99@gmail.com>
2023-11-04 06:02:15 +0200

    Yes voting

--- Blockend/DAO.sol
+++ Blockend/DAO.sol
@@ -90,7 +90,21 @@ contract DAO {
         emit ProposalCreated(proposals.length - 1, _description);
     }
 
-    // Actual vote
+    // Actual vote 
+    //  yesVoting
+    function voteYes(uint _proposalId, uint _tokenAmount) public {
+        require(isMember[msg.sender] == true, "You should be a member to vote");
+        require(balances[msg.sender] >= _tokenAmount, "Not enough tokens to vote");
+        require(votes[msg.sender][_proposalId] == false, "You have already voted for this proposal");
+        votes[msg.sender][_proposalId] = true;
+        memberInfo[msg.sender].tokenBalance -= _tokenAmount;
+        proposals[_proposalId].voteCount += _tokenAmount;
+        proposals[_proposalId].yesVotes +=_tokenAmount;
+
+        emit VoteCast(msg.sender, _proposalId, _tokenAmount);
+    }
+
+    // vite no
     
 
 
