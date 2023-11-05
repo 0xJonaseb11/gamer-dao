2afc4a769061e0d5d098e0cc99b770af2fe18546
jonas-sebera <sebejaz99@gmail.com>
2023-11-05 21:35:52 +0200

    getting done with lib imports

--- Client/Q-BoilerPlate/contracts/AirDropV1.sol
+++ Client/Q-BoilerPlate/contracts/AirDropV1.sol
@@ -1,2 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+
+import {MerkleWhitelisted} from "@dlsl/dev-modules/access-control/MerkleWhitelisted.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {TokenBalance} from "./libs/TokenBalance.sol";
