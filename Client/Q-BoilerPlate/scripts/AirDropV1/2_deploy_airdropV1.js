007f05247a983df7b5061581efd01c196ab64778
jonas-sebera <sebejaz99@gmail.com>
2023-11-05 22:01:57 +0200

    init scripts

---
+++ Client/Q-BoilerPlate/scripts/AirDropV1/2_deploy_airdropV1.js
@@ -0,0 +1,6 @@
+const { ethers }  = require("hardhat");
+Web3 = require("web3");
+const keccak256 = require("keccak256");
+const ( merkleTree ) = require("merkletreejs");
+const fs = require("fs");
+
