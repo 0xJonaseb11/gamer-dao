916bb5e0bd50a95faa2f6b73ddd6b997227e500e
jonas-sebera <sebejaz99@gmail.com>
2023-11-05 21:41:11 +0200

    AirDropV1 init

--- Client/Q-BoilerPlate/contracts/AirDropV1.sol
+++ Client/Q-BoilerPlate/contracts/AirDropV1.sol
@@ -5,3 +5,18 @@ pragma solidity ^0.8.19;
 import {MerkleWhitelisted} from "@dlsl/dev-modules/access-control/MerkleWhitelisted.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {TokenBalance} from "./libs/TokenBalance.sol";
+
+
+contract AirDropV1 is MerkleWhiteListed, Ownable {
+
+    // events
+
+    event RewardClaimed(bytes32 indexed merkelRoot, address indexed account);
+    event AirDropCreated(bytes32 indexed merkleRoot, address indexed rewardToken, uint256 rewardAmount);
+
+    // state variables
+    address public rewardToken;
+    uint256 public rewardAmount;
+    mapping(bytes32 => mapping(address => bool)) public isUserClaimed;
+    
+}
