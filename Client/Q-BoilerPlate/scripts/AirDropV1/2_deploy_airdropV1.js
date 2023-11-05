6263d3d4144782520055e24c1fc357a0125fae3c
jonas-sebera <sebejaz99@gmail.com>
2023-11-05 22:04:43 +0200

    added constants

--- Client/Q-BoilerPlate/scripts/AirDropV1/2_deploy_airdropV1.js
+++ Client/Q-BoilerPlate/scripts/AirDropV1/2_deploy_airdropV1.js
@@ -1,6 +1,10 @@
 const { ethers }  = require("hardhat");
 Web3 = require("web3");
 const keccak256 = require("keccak256");
-const ( merkleTree ) = require("merkletreejs");
+const { merkleTree } = require("merkletreejs");
 const fs = require("fs");
 
+/// add contracts
+const qrc20Address = "0x98229ae26542F6061b69159a628e2990De12A7fA";
+const QRC20 = await ethers.getContractFactory("QRC20");
+const contract = QRC20.attach(qrc20Address);
