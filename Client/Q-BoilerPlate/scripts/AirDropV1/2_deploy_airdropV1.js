77974157d79961590f9aafd9a3f1a9a56dc47772
jonas-sebera <sebejaz99@gmail.com>
2023-11-05 22:22:10 +0200

    deployed with airdropV1 init

--- Client/Q-BoilerPlate/scripts/AirDropV1/2_deploy_airdropV1.js
+++ Client/Q-BoilerPlate/scripts/AirDropV1/2_deploy_airdropV1.js
@@ -1,9 +1,11 @@
 const { ethers }  = require("hardhat");
 Web3 = require("web3");
 const keccak256 = require("keccak256");
-const { merkleTree, default: MerkleTree } = require("merkletreejs");
+const { MerkleTree } = require("merkletreejs");
 const fs = require("fs");
-const { root } = require("postcss");
+
+
+async function main () {
 
 /// add contracts
 const qrc20Address = "0x98229ae26542F6061b69159a628e2990De12A7fA";
@@ -31,3 +33,30 @@ const data = {
 };
 
 fs.writeFileSync("tree.json", JSON.stringify(data, null, 2));
+
+
+// Deploying contract
+console.log("Deploying AirdropV1...");
+
+const AirDropV1 = await ethers.getContractFactory("AirDropV1");
+const dropAmt = Web3.utils.toWei("20", "ether");
+const airdrop = await AirDropV1.deploy();
+
+console.log("AirDropV1 deployed to: ", await airdrop.getAddress());
+
+await airdrop.create_airdrop(qrc20Address, dropAmt, root);
+
+console.log("Funding the Airdrop");
+
+
+// mint tokens
+const airdropAddress =  airdrop.getAddress();
+const mintAmount = Web3.utils.toWei("2000", "ether");
+
+await contract.mintTo(airdropAddress, mintAmount);
+
+console.log("Airdrop Funded...");
+
+}
+
+main();
