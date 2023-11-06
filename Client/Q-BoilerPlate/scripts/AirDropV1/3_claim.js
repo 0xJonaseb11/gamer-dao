04907b5dd9fa9f7e065309686308d3c69ac7b49d
jonas-sebera <sebejaz99@gmail.com>
2023-11-06 09:31:14 +0200

    imported Airdrop contract

--- Client/Q-BoilerPlate/scripts/AirDropV1/3_claim.js
+++ Client/Q-BoilerPlate/scripts/AirDropV1/3_claim.js
@@ -5,3 +5,17 @@ const { MerkleTree } = require("merkletreejs");
 web3 = require("web3"); 
 const fs = require("fs");
 
+async function main () {
+    
+    // address of deployed aidrop contract
+    const Airdrop = "0x604d5f96Fd498647FD2C2Bf1E14E952db1409F22";
+    const Airdrop_fac = await ethers.getContractFactory("AirDropV1");
+
+    // attach contract instance to the deployed contract
+    const contract = Airdrop_fac.attach(Airdrop);
+
+    
+
+}
+
+main();
