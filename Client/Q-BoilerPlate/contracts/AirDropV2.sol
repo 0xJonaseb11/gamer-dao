7f5fdbc9c62a400589cb8dcf7c962fc43321dda6
jonas-sebera <sebejaz99@gmail.com>
2023-11-06 12:27:11 +0200

    create campaign

--- Client/Q-BoilerPlate/contracts/AirDropV2.sol
+++ Client/Q-BoilerPlate/contracts/AirDropV2.sol
@@ -18,4 +18,21 @@ contract AirDropV2 is ACampaignAirDrop, Initializable, IDAOResource {
         require(msg.sender == votingContract, "AirDropV2: caller is not contract.");
         _;
     }
+
+    // constructor
+    function __AirDropV2_init(address votingContract_) public initializer {
+        votingContract = votingContract_;
+    }
+
+    function createCampaign(
+        address rewardToken_,
+        uint256 rewardAmount_,
+        bytes32 merkleRoot_,
+        uint256 startTimestamp_,
+        uint256 endingTimestamp,
+    ) external onlyVotingContract returns (uint256) {
+        return _createCampaign(rewardToken_, rewardAmount_, startingTimestamp_, endingTimestamp_, merkleRoot_ );
+    }
+
+    
 }
