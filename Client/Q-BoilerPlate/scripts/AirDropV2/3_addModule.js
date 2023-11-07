cad72629d76e79635da448f395da78af10ad2685
jonas-sebera <sebejaz99@gmail.com>
2023-11-07 13:30:25 +0200

    init organise

--- Client/Q-BoilerPlate/scripts/AirDropV2/3_addModule.js
+++ Client/Q-BoilerPlate/scripts/AirDropV2/3_addModule.js
@@ -1,28 +1,32 @@
 const { getEncodedData } = require("@q-dev/gdk-sdk");
 const { ethers } = require("hardhat");
-require("dotenv").config();
+require('dotenv').config();
 
+// Retrieving environment variables
 const { MODULE_IMPLEMENTATION, MODULE_NAME, MAIN_DAO_VOTING_ADDRESS } = process.env;
 
-// init main function
+// The main function for deploying the voting proposal
 async function main() {
+  // Retrieving Ethereum accounts
+  const accounts = await ethers.getSigners();
+  const senderAddress = accounts[0].address;
 
-    // Retrieving eth accounts
-    const accounts = await ethers.getSigners();
-    const senderAddress = accounts[0].address;
+  // Get the contract factories for GeneralDAOVoting
+  const GeneralDAOVotingFactory = await ethers.getContractFactory("GeneralDAOVoting");
 
-    const GeneralDAOVotingFactory = await ethers.getContractFactory("GeneralDAOVoting");
-    const VotingContract = GeneralDAOVotingFactory.attach(MAIN_DAO_VOTING_ADDRESS);
-     
-    // CREATE encoded data
-    const addModulecalldata = getEncodedData("DAORegistry", "addProxyContract", MODULE_NAME, MODULE_IMPLEMENTATION);
+  // Attach to existing instances of GeneralDAOVoting
+  const VotingContract = GeneralDAOVotingFactory.attach(MAIN_DAO_VOTING_ADDRESS);
 
-    // create proposal
-    console.log(`Create Proposal to add ${MODULE_NAME} Module to the DAO Registry`);
-    await VotingContract.createProposal("DAORegistry", `Adding ${MODULE_NAME} To the DAO Contract Registry`, addModulecalldata, {
-        from: senderAddress,
-    });
+  // Creating encoded calldata to add the new module to the DAO Registry
+  const addModuleCalldata = getEncodedData("DAORegistry", "addProxyContract", MODULE_NAME, MODULE_IMPLEMENTATION);
 
-    console.log("Proposal Created");
+  // Creating a voting proposal to add the module to the DAO Registry
+  console.log(`Create Proposal to add ${MODULE_NAME} module to the DAO Registry`);
+  await VotingContract.createProposal("DAORegistry", `Adding ${MODULE_NAME} to the DAO Contract Registry`, addModuleCalldata, {        
+    from: senderAddress,
+  });
 
+  console.log("Proposal Created");
 }
+
+main();
