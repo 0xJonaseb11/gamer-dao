3ea81db9876ef78df19a4b7305df0dc14a169c14
jonas-sebera <sebejaz99@gmail.com>
2023-11-17 16:37:28 +0200

    init update

--- Smart_contract/Q-BoilerPlate/contracts/QRC20 copy.sol
+++ Smart_contract/Q-BoilerPlate/contracts/QRC20 copy.sol
@@ -16,3 +16,35 @@ import "@q-dev/gdk-contracts/metadata/ContractMetadata.sol";
  * - total supply cap
  * - contract metadata
  */
+
+
+ contract QRC20 is IQRC20, ERC20Upgradeable, ContractMetadata, OwnableUpgradeable {
+    string public QRC20_RESOURCE;
+    uint256 public totalSupplyCap();
+    uint256 internal _decimals;
+
+
+    function initialize (
+        string calldata name_,
+        string calldata symbol_,
+        uint8 deimals_,
+        string calldata contractURI_,
+        string calldata resource_,
+        uint256 totalSupplyCap_
+
+    ) public  initializer {
+        _ERC20_init(name_, symbol_);
+        _ContractMetadata_init(contractURI_);
+        QRC20_RESOURCE = resource_;
+        _decimals = decimals_;
+        totalSupplyCap = totalSupplyCap_;
+
+        // set owner
+        __Ownable_init();
+    }
+
+    modifier mintTo(address account, uint256 amount) external override onlyOwner {
+        require(totalSupplyCap == 0 || totalSupply() + amount <= totalSupplyCap, "[QGDK-015000]-The total supply capacity exceeded, minting is not allowed.");
+        
+    }
+ }
