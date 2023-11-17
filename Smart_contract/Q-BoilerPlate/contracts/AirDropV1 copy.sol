ceb6f665602562c4dd77f6467ff41aa89cf4954d
jonas-sebera <sebejaz99@gmail.com>
2023-11-17 16:59:37 +0200

    init update

--- Smart_contract/Q-BoilerPlate/contracts/AirDropV1 copy.sol
+++ Smart_contract/Q-BoilerPlate/contracts/AirDropV1 copy.sol
@@ -8,5 +8,22 @@ import {TokenBalance} from "./libs/TokenBalance.sol";
 
 
 contract AirDropV1 is MerkleWhitelisted, Ownable {
-    
+
+    event RewardClaimed(bytes32 indexed merkleRoot, address indexed account);
+    event AirDropCreated(bytes32 indexed merkelRoot, address indexed rewaardToken, uint256 rewardAmount);
+
+
+    // state variables
+    address public rewardToken;
+    uint256 public rewardAmount;
+
+    mapping(bytes32 => mapping(address => bool)) public isUserClaimed;
+
+    modifier onlyNotClaimed(address account_) {
+        require(
+            !isUserClaimed[getMerkleRoot()][account_],
+            "AirDropV1: Account already claimed reward."
+        );
+        _;
+    }
 }
