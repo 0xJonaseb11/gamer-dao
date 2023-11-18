5fe80b7af6a9fb2c845638d69bc2bb125b50a53e
jonas-sebera <sebejaz99@gmail.com>
2023-11-18 22:17:04 +0200

    updated

--- Smart_contract/Q-BoilerPlate/contracts/DAO copy.sol
+++ Smart_contract/Q-BoilerPlate/contracts/DAO copy.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.19;
 
 contract DAO {
-
-    struct Poposal {
+   
+      // struct for proposal
+    struct Proposal {
         string description;
         uint voteCount;
         uint yesVotes;
@@ -11,10 +12,28 @@ contract DAO {
         bool executed;
     }
 
-
+        // struct for member
     struct Member {
         address memberAddress;
         uint memberSince;
         uint tokenBalance;
     }
+
+    // state variables
+    address[] public members;
+
+    mapping(address => bool) public isMember;
+    mapping(address => Member) public memberInfo;
+    mapping(address => mapping(uint => bool)) public votes;
+
+    Proposal[] public proposals;
+    address public owner;
+
+    event ProposalCreated(uint indexed proposalId, string description);
+    event VoteCast(address indexed voter, uint indexed proposalId, uint tokenAmount);
+    event ProposalAccepted(string message);
+    event ProposalRejected(string rejected);
+
+
+
 }
