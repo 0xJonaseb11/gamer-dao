5e200365d161b12f5516897e044c599204279377
Jaz-3-0 <sebejaz99@gmail.com>
2023-12-23 10:58:57 +0200

    init update

--- Client/src/context/Web3ContextProvider/Web3ContextProvider copy.tsx
+++ Client/src/context/Web3ContextProvider/Web3ContextProvider copy.tsx
@@ -3,11 +3,12 @@ import { createContext, FC, ReactElement, useContext, useEffect, useMemo, useSta
 
 import { useLocalStorage } from "@q-dev/react-hooks";
 import { ErrorHandler } from "helpers";
-import { UseProvider } from "typings";
+import { UseProvider, useProvider, useWeb3 } from "hooks";
 import { Wrap } from "./styles";
 
 import { chainIdToNetworkMap, networkConfigsMap, ORIGIN_NETWORK_NAME } from "constants/config";
 import { PROVIDERS } from "constants/providers";
+import { Web3Context } from "./Web3ContextProvider";
 
 export type Web3Data = {
   currentProvider: UseProvider;
@@ -19,3 +20,13 @@ export type Web3Data = {
   disconnect: () => void;
 };
 
+export const Web3Context = createContext({} as Web3Data);
+
+const Web3ContextProvider: FC<{ children: ReactElement }>  = ({ children }) => {
+  const web3 = useWeb3();
+  const metamaskProvider = useProvider();
+  const defaultProvider = useProvider();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoadFailed, setIsLoadFailed] = useState(false);
+  const [selectedProvider, setSelectedProvider] = useLocalStorage<undefined | PROVIDERS>('selectedProvider', undefined);
+}
