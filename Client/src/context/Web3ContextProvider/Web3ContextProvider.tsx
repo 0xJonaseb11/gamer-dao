6d5c3314be8e209ff2cbbceb36ab9a45c563a397
Jaz-3-0 <sebejaz99@gmail.com>
2023-12-23 16:36:55 +0200

    init update

--- Client/src/context/Web3ContextProvider/Web3ContextProvider.tsx
+++ Client/src/context/Web3ContextProvider/Web3ContextProvider.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { createContext, FC, ReactElement, useContext, useEffect, useMemo, useState } from 'react';
+
 
 import { useLocalStorage } from '@q-dev/react-hooks';
 import { ErrorHandler } from 'helpers';
