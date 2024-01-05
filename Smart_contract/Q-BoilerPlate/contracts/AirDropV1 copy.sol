3bec6ab9700337bd269da64b4fa803ee2a1a22ef
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-05 16:19:23 +0200

    init update airdrop_II

---
+++ Smart_contract/Q-BoilerPlate/contracts/AirDropV1 copy.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// import external modules for access control and token balance handling
+import { MerkleWhitelisted } from "@dlsl/dev-modules/access-control/MerkleWhitelisted.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { TokenBalance } from "./libs/TokenBalance.sol";
+
+
