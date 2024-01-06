0dacb5a8e276cd879f13a7d995247e3caf57c6c5
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-06 08:28:03 +0200

    fully-initialize contract with metadata

--- Smart_contract/Q-BoilerPlate/contracts/QRC20 copy.sol
+++ Smart_contract/Q-BoilerPlate/contracts/QRC20 copy.sol
@@ -22,5 +22,18 @@ contract QRC20 is IQRC20, ERC20Upgradeable, ContractMetadata, OwnableUpgradeable
     uint256 public totalSupplyCap; // Maximum total supply cap of the token
     uint8 internal _decimals; // Number of decimals for the token
 
-    // 
+    // Initilize contract with provided metadata and configuration
+    function initialize(
+        string calldata name_, string calldata symbol_, uint8 decimals_,
+        string calldata contractURI_, string calldata resource_, uint256 totalSupplyCap_
+    ) public initializer {
+        __ERC20_init(name_, symbol); // Initialize ERC20 token with name and symbol
+        __ContractMetadata_init(contractURI_); // initialize contract metadata with URI
+        QRC20_RESOURCE = resource_; // set QRC20 resource
+        _decimals = decimals_; // set token decimals
+        totalSupplyCap = totalSupplyCap_;
+
+        // set contract owner
+        __Ownable_init();
+    }
 }
