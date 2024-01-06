b6d7a9153d17fde5b4a919942f69ca25b506e8b7
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-06 07:51:12 +0200

    init update

--- Smart_contract/Q-BoilerPlate/contracts/AirDropV2 copy.sol
+++ Smart_contract/Q-BoilerPlate/contracts/AirDropV2 copy.sol
@@ -7,5 +7,22 @@ import { IDAOResource } from "@q-dev/gdk-contracts/interfaces/IDAOResource.sol";
 import { ACampaignAirDrop } from "./libs/ACampaignAirDrop.sol";
 
 contract AirDropV2 is ACampaignAirDrop, Initializable, IDAOResource {
-    
+
+    // state variables
+    string public constant AIR_DROP_V2_RESOURCE = "AIR_DROP_V2";
+    address public votingContract;
+
+    // modifier to initialize contract
+    modifier onlyVotingContract() {
+        require(msg.sender == votingContract, "AirDropV2: Caller not contract");
+        _;
+    }
+
+    // constructor
+
+     // Initialize contract
+     function __AirDropV2_init(address votingContract_) public {
+        votingContract = votingContract_;
+     }
+     
 }
