bf1d5624e6c1ca5b310f3d481cf831be34c55d1a
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-06 08:20:19 +0200

    QRC20 INIT

--- Smart_contract/Q-BoilerPlate/contracts/QRC20 copy.sol
+++ Smart_contract/Q-BoilerPlate/contracts/QRC20 copy.sol
@@ -8,3 +8,19 @@ import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC2
 import { IQRC20 } from "@q-dev/gdk-contracts/interfaces/tokens/IQRC20.sol";
 import { ContractMetadata } from "@q-dev/gdk-contracts/metadata/ContractMetadata.sol";
 
+/**
+ * @title QRC20
+ * 
+ * Regular QRC20 token with additional features
+ * - minting and burning
+ * total supply cap
+ * contravt metadata
+ */
+
+contract QRC20 is IQRC20, ERC20Upgradeable, ContractMetadata, OwnableUpgradeable {
+    string public QRC20_RESOURCE; // Resource of the QRC20 token
+    uint256 public totalSupplyCap; // Maximum total supply cap of the token
+    uint8 internal _decimals; // Number of decimals for the token
+
+    // 
+}
