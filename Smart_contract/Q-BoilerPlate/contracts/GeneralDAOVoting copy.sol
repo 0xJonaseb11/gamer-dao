19e18f1256b0e011420b53a1ac4b16836e0720d0
Jaz-3-0 <sebejaz99@gmail.com>
2024-01-07 09:36:59 +0200

    init state variables

--- Smart_contract/Q-BoilerPlate/contracts/GeneralDAOVoting copy.sol
+++ Smart_contract/Q-BoilerPlate/contracts/GeneralDAOVoting copy.sol
@@ -27,6 +27,32 @@ contract GenealDAOVoting is IDAOVoting, Initializable, AbstractDepandant {
     using StringSet for StringSet.Set;
 
     // state variables
+    string public constant VOTING_PERIOD = "VotingPeriod";
+    string public constant VETO_PERIOD = "VetoPeriod";
+    string public constant PROPOSAL_EXECUTION_PERIOD = "proposalExecutionPeriod";
+    string public constant REQUIRED_QUORUM = "requiredQuorum";
+    string public constant REQUIRED_MAJORITY = "requiredMajority";
+    string public constant REQUIRED_VETO_QUORUM = "requiredVetoQuorum";
+    string public constant VOTING_TYPE = "otingType";
+    string public constant VOTING_TARGET = "votingTarget";
+    string public constant VOTING_MIN_AMOUNT = "votingMinAmount";
+
+    string public DAO_VOTING_RESOURCE;
+    string public targetPanel;
+    address public votingToken;
+    uint256 public proposalCount;
+
+    DAOVault public daoVault;
+    DAORegistry public daoRegistry;
+    PermissionManager public permissionManager;
+    DAOParameterStorage public daoParameterStorage;
+
+    StringSet.Set internal _votingSituations;
+
+    mapping(uint256 => DAOProposal) public proposals;
+    mapping(uint256 => mapping(address => bool)) public hasUserVoted;
+    mapping(uint256 => mapping(address => bool)) public hasUserVetoed;
+
     
 
 }
